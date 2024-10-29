84e98f49e8e62f58c4e22a89ba0c9bc45d6137e9
NabeelShajahan <130592372+NabeelShajahan@users.noreply.github.com>
2024-10-29 19:43:24 -0400

    Initial Commit

---
+++ KindMeal/KindMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  KindMealApp.swift
+//  KindMeal
+//
+//  Created by Nabeel Shajahan on 2024-10-29.
+//
+
+import SwiftUI
+
+@main
+struct KindMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KindMealTests/KindMealTests.swift
@@ -0,0 +1,17 @@
+//
+//  KindMealTests.swift
+//  KindMealTests
+//
+//  Created by Nabeel Shajahan on 2024-10-29.
+//
+
+import Testing
+@testable import KindMeal
+
+struct KindMealTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
