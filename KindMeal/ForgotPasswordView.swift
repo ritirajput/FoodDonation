021caff56ad7ceb72501806ff75eec2864fd545d
NabeelShajahan <130592372+NabeelShajahan@users.noreply.github.com>
2024-10-30 22:15:09 -0400

    updated the forgotpassword

--- KindMeal/ForgotPasswordView.swift
+++ KindMeal/ForgotPasswordView.swift
@@ -6,10 +6,54 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
+
 
 struct ForgotPasswordView: View {
+    @State private var email = ""
+    @State private var message = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Reset Password")
+                .font(.title)
+                .padding()
+
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.emailAddress)
+                .autocapitalization(.none)
+                .padding(.horizontal)
+
+            Button(action: resetPassword) {
+                Text("Send Reset Link")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.orange)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .padding(.horizontal)
+            .padding(.top, 20)
+
+            Text(message)
+                .foregroundColor(.green)
+                .padding()
+
+            Spacer()
+        }
+        .padding()
+    }
+
+    func resetPassword() {
+        Auth.auth().sendPasswordReset(withEmail: email) { error in
+            if let error = error {
+                self.message = error.localizedDescription
+            } else {
+                self.message = "Password reset email sent!"
+            }
+        }
     }
 }
 
