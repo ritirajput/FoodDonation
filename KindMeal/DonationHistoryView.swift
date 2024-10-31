22bc6685992763109c03bc4b836d512f29811b15
NabeelShajahan <130592372+NabeelShajahan@users.noreply.github.com>
2024-10-31 15:25:41 -0400

    Initialised firebase and created the donationHistory page

---
+++ KindMeal/DonationHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  DonationHistoryView.swift
+//  KindMeal
+//
+//  Created by Nabeel Shajahan on 2024-10-31.
+//
+
+import SwiftUI
+
+struct DonationHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DonationHistoryView()
+}
