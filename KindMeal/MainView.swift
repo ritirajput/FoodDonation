e8183ff129c4d4162f6b93798199182e77efa3dc
NabeelShajahan <130592372+NabeelShajahan@users.noreply.github.com>
2024-10-31 03:09:17 -0400

    the dashboard view for donors and created the makeDonation View

--- KindMeal/MainView.swift
+++ KindMeal/MainView.swift
@@ -8,10 +8,47 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var showingDonationForm = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Welcome to KindMeal")
+                    .font(.title)
+                    .padding()
+
+                Button(action: {
+                    showingDonationForm = true
+                }) {
+                    Text("Make a Donation")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .padding()
+
+                NavigationLink(destination: DonationHistoryView()) {
+                    Text("View Donation History")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .padding()
+
+                Spacer()
+            }
+            .sheet(isPresented: $showingDonationForm) {
+                MakeDonationView()
+            }
+            .navigationTitle("Dashboard")
+        }
     }
 }
+
 
 #Preview {
     MainView()
---
+++ KindMeal/MakeDonationView.swift
@@ -0,0 +1,18 @@
+//
+//  MakeDonationView.swift
+//  KindMeal
+//
+//  Created by Nabeel Shajahan on 2024-10-31.
+//
+
+import SwiftUI
+
+struct MakeDonationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MakeDonationView()
+}
