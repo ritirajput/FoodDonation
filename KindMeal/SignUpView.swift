7b672e178f2451746b167e79e49e6d1ed8532a38
NabeelShajahan <130592372+NabeelShajahan@users.noreply.github.com>
2024-11-28 08:43:58 -0500

    app theme for the signup page

--- KindMeal/SignUpView.swift
+++ KindMeal/SignUpView.swift
@@ -19,107 +19,106 @@ struct SignUpView: View {
     @State private var errorMessage: String = ""
     @State private var showAlert: Bool = false
     @State private var navigateToLoginView: Bool = false
-
+    
     private let genders = ["Male", "Female", "Other"]
     private var databaseRef: DatabaseReference = Database.database().reference()
-
+    
     var body: some View {
         ZStack {
-            Image("background_signup")
-                .resizable()
-                .scaledToFit()
-                .edgesIgnoringSafeArea(.all)
-
+            // Background color
+            Color(red: 229/255, green: 255/255, blue: 192/255)
+                .ignoresSafeArea()
+            
             ScrollView {
                 VStack {
+                    // Logo
+                    Image("Connecting Hearts Through Meals. (1)") // Replace with the correct logo name in Assets
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 150)
+                        .padding(.top, 20)
+                    
+                    // Title
                     Text("Create Your Account")
-                        .font(.custom("roboto_serif_regular", size: 32))
-                        .foregroundColor(.black)
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(.green)
+                        .padding(.top, 10)
                         .padding(.bottom, 20)
-
-                    Text("Username")
-                        .foregroundColor(.black)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal)
-                    TextField("Enter your username", text: $username)
-                        .padding()
-                        .background(Color.white.opacity(0.8))
-                        .cornerRadius(10)
-                        .padding(.horizontal)
-
-                    Text("Email")
-                        .foregroundColor(.black)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal)
-                    TextField("Enter your email", text: $email)
-                        .padding()
-                        .background(Color.white.opacity(0.8))
-                        .cornerRadius(10)
+                    
+                    // Username Field
+                    customInputField(title: "Username", placeholder: "Enter your username", text: $username)
+                    
+                    // Email Field
+                    customInputField(title: "Email", placeholder: "Enter your email", text: $email)
+                        .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .disableAutocorrection(true)
-                        .padding(.horizontal)
-
-                    Text("Password")
-                        .foregroundColor(.black)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal)
-                    SecureField("Enter your password", text: $password)
-                        .padding()
-                        .background(Color.white.opacity(0.8))
-                        .cornerRadius(10)
-                        .padding(.horizontal)
-
-                    Text("Confirm Password")
-                        .foregroundColor(.black)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal)
-                    SecureField("Re-enter your password", text: $confirmPassword)
-                        .padding()
-                        .background(Color.white.opacity(0.8))
-                        .cornerRadius(10)
-                        .padding(.horizontal)
-
-                    Text("Phone Number")
-                        .foregroundColor(.black)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal)
-                    TextField("Enter your phone number", text: $phoneNumber)
-                        .padding()
-                        .background(Color.white.opacity(0.8))
-                        .cornerRadius(10)
+                    
+                    // Password Field
+                    customSecureField(title: "Password", placeholder: "Enter your password", text: $password)
+                    
+                    // Confirm Password Field
+                    customSecureField(title: "Confirm Password", placeholder: "Re-enter your password", text: $confirmPassword)
+                    
+                    // Phone Number Field
+                    customInputField(title: "Phone Number", placeholder: "Enter your phone number", text: $phoneNumber)
                         .keyboardType(.phonePad)
-                        .padding(.horizontal)
-
-                    Text("Gender")
-                        .foregroundColor(.black)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal)
-                    Picker("Select Gender", selection: $selectedGender) {
-                        ForEach(genders, id: \.self) { gender in
-                            Text(gender)
+                    
+                    // Gender Picker
+                    VStack(alignment: .leading) {
+                        Text("Gender")
+                            .foregroundColor(.black)
+                            .padding(.horizontal)
+                            .font(.headline)
+                        Picker("Select Gender", selection: $selectedGender) {
+                            ForEach(genders, id: \.self) { gender in
+                                Text(gender)
+                            }
                         }
+                        .padding()
+                        .background(Color.white.opacity(0.8))
+                        .cornerRadius(15)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.green, lineWidth: 1)
+                        )
+                        .padding(.horizontal)
                     }
-                    .padding()
-                    .background(Color.white.opacity(0.8))
-                    .cornerRadius(10)
-                    .padding(.horizontal)
-
+                    .padding(.top, 10)
+                    
+                    // Register Button
                     Button(action: signUpUser) {
                         Text("Register")
+                            .font(.headline)
                             .frame(maxWidth: .infinity)
                             .padding()
                             .background(Color.green)
                             .foregroundColor(.white)
-                            .cornerRadius(10)
+                            .cornerRadius(15)
+                            .shadow(radius: 5)
                     }
                     .padding(.horizontal)
                     .padding(.top, 20)
-
-                    Text(errorMessage).foregroundColor(.red)
+                    
+                    // Error Message
+                    if !errorMessage.isEmpty {
+                        Text(errorMessage)
+                            .foregroundColor(.red)
+                            .font(.footnote)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 20)
+                    }
+                    
+                    // Spacer for layout
                     Spacer()
-
+                    
+                    // Navigation Link to Login
                     NavigationLink(destination: LoginView(), isActive: $navigateToLoginView) {
-                        EmptyView()
+                        Text("Already have an account? Log In")
+                            .foregroundColor(.blue)
+                            .font(.footnote)
+                            .padding(.top, 20)
                     }
                 }
                 .padding()
@@ -129,7 +128,48 @@ struct SignUpView: View {
             Alert(title: Text("Error"), message: Text(errorMessage), dismissButton: .default(Text("OK")))
         }
     }
-
+    
+    // Function to create reusable input fields
+    private func customInputField(title: String, placeholder: String, text: Binding<String>) -> some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .foregroundColor(.black)
+                .padding(.horizontal)
+                .font(.headline)
+            TextField(placeholder, text: text)
+                .padding()
+                .background(Color.white.opacity(0.8))
+                .cornerRadius(15)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color.green, lineWidth: 1)
+                )
+                .padding(.horizontal)
+        }
+        .padding(.top, 10)
+    }
+    
+    // Function to create reusable secure fields
+    private func customSecureField(title: String, placeholder: String, text: Binding<String>) -> some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .foregroundColor(.black)
+                .padding(.horizontal)
+                .font(.headline)
+            SecureField(placeholder, text: text)
+                .padding()
+                .background(Color.white.opacity(0.8))
+                .cornerRadius(15)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color.green, lineWidth: 1)
+                )
+                .padding(.horizontal)
+        }
+        .padding(.top, 10)
+    }
+    
+    // Sign-Up Functionality
     private func signUpUser() {
         // Validate input fields
         guard !username.isEmpty else {
@@ -137,8 +177,18 @@ struct SignUpView: View {
             showAlert = true
             return
         }
+        guard !email.isEmpty else {
+            errorMessage = "Email is required"
+            showAlert = true
+            return
+        }
         guard email.isValidEmail() else {
-            errorMessage = "Invalid email format"
+            errorMessage = "Invalid email format. Please enter a valid email."
+            showAlert = true
+            return
+        }
+        guard !password.isEmpty else {
+            errorMessage = "Password is required"
             showAlert = true
             return
         }
@@ -147,8 +197,13 @@ struct SignUpView: View {
             showAlert = true
             return
         }
+        guard !phoneNumber.isEmpty else {
+            errorMessage = "Phone number is required"
+            showAlert = true
+            return
+        }
         guard phoneNumber.isValidPhone() else {
-            errorMessage = "Invalid phone number"
+            errorMessage = "Invalid phone number. Must be 10 digits."
             showAlert = true
             return
         }
@@ -157,7 +212,7 @@ struct SignUpView: View {
             showAlert = true
             return
         }
-
+        
         // Create user with Firebase Authentication
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             if let error = error {
@@ -165,18 +220,17 @@ struct SignUpView: View {
                 showAlert = true
                 return
             }
-
+            
             guard let uid = authResult?.user.uid else { return }
-
+            
             // Prepare user data
             let userData: [String: Any] = [
                 "username": username,
                 "email": email,
                 "phoneNumber": phoneNumber,
                 "gender": selectedGender,
-                "role": "user"
             ]
-
+            
             // Save user data to Firebase Realtime Database
             databaseRef.child("users").child(uid).setValue(userData) { error, _ in
                 if let error = error {
@@ -193,8 +247,8 @@ struct SignUpView: View {
 
 extension String {
     func isValidEmail() -> Bool {
-        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
+        let emailFormat = "(?:[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64})"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailFormat)
         return emailPredicate.evaluate(with: self)
     }
 
@@ -205,3 +259,6 @@ extension String {
     }
 }
 
+#Preview {
+    SignUpView()
+}
