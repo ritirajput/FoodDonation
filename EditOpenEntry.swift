d2ed89d1a3ac33e46e34b257488cda7b48a608ec
ritirajput <ritiraj3022@gmail.com>
2025-01-15 19:55:37 -0500

    Edit open entry

---
+++ EditOpenEntry.swift
@@ -0,0 +1,18 @@
+//
+//  EditOpenEntry.swift
+//  KindMeal
+//
+//  Created by Admin on 2025-01-08.
+//
+
+import SwiftUI
+
+struct EditOpenEntry: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditOpenEntry()
+}
